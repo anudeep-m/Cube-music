91b7aca4946209765cf9b3fbb7a139a278b4fed7
Anudeep Muktinutalapati <anudeep.mp@yahoo.com>
2021-09-12 20:14:00 +0530

    Production static assets update

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,19 @@ const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 app.use('/frontend', express.static(path.join(__dirname, '/frontend')))
 
+// //Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 //Middlewares****
 app.use(notFound)
 app.use(errorHandler)
